58b3492b56516d5a893ebe2715db718a8344595c
jkr <jimmy.kjaersgaard@gmail.com>
2017-04-09 23:03:19 +0200

    knit subsite works. Converted all calculations to js to avoid POST. Calculations are now done "live" when slider is moved.

--- knitting/forms.py
+++ knitting/forms.py
@@ -7,10 +7,14 @@ class CreaseForm(forms.Form):
         validators=[MinValueValidator(1)],
         widget=forms.NumberInput(attrs={'type':'range',
                                         'class':'knit-slider',
+                                        'min':10,
+                                        'max':400,
                                         'name': "from_range"}))
     N_to = forms.IntegerField(
         label='To',
         validators=[MinValueValidator(1)],
         widget=forms.NumberInput(attrs={'type':'range',
                                         'class':'knit-slider',
+                                        'min':10,
+                                        'max':400,
                                         'name': "to_range"}))
